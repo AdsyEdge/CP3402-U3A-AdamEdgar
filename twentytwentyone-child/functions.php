e44272573410d16124095f0975e7ccf394bcb51a
AdsyEdge <adam.edgar@my.jcu.edu.au>
2023-07-17 10:31:37 +1000

    Updated functions.php, style.css

--- twentytwentyone-child/functions.php
+++ twentytwentyone-child/functions.php
@@ -1,6 +1,15 @@
 <?php
-function enqueue_child_theme_styles() {
-   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), wp_get_theme()->get('Version') );
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    $parenthandle = 'parent-style'; // This is 'twentytwentytwo-style' for the Twenty Fifteen theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(),  // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles' );
 ?>
