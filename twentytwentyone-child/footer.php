3d1a5147ee79e94d05df6163a866ae54d3b72b24
AdsyEdge <adam.edgar@my.jcu.edu.au>
2023-07-19 01:33:14 +1000

    updated style.css, footer.php, remove footer-widgets.php

--- twentytwentyone-child/footer.php
+++ twentytwentyone-child/footer.php
@@ -15,30 +15,8 @@
 			</main><!-- #main -->
 		</div><!-- #primary -->
 	</div><!-- #content -->
-
-	<?php get_template_part( 'template-parts/footer/footer-widgets' );?>
-
 	<footer id="colophon" class="site-footer">
 
-		<?php if ( has_nav_menu( 'footer' ) ) : ?>
-			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
-				<ul class="footer-navigation-wrapper">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</ul><!-- .footer-navigation-wrapper -->
-			</nav><!-- .footer-navigation -->
-		<?php endif; ?>
 		<div class="site-info">
 			<div class="site-name">
 				<?php if ( has_custom_logo() ) : ?>
@@ -49,8 +27,6 @@
 					<?php endif; ?>
 				<?php endif; ?>
 			</div><!-- .site-name -->
-
-			<p>Hello, World!</p>
 
 			<div class="powered-by">
 				<p>Designed by <a href="https://github.com/AdsyEdge">Adam Edgar</a></p>
--- twentytwentyone-child/template-parts/footer/footer-widgets.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Displays the footer widget area.
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
-?>
